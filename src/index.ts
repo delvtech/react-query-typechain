412aa4d22283dffb04d87b45d45b4109c113dcd8
Danny Delott <dannydelott@gmail.com>
2022-02-01 14:48:51 -0800

    Move file out of hooks/ directory

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export * from "src/base/TransactionError";
 export * from "src/base/TransactionStatus";
 export * from "src/base/Unpacked";
 
-export * from "src/hooks/matchSmartContractReadCallQuery/matchSmartContractReadCallQuery";
+export * from "src/utils/matchSmartContractReadCallQuery/matchSmartContractReadCallQuery";
 export * from "src/hooks/useSmartContractEvents/useSmartContractEvents";
 export * from "src/hooks/useSmartContractReadCall/useSmartContractReadCall";
 export * from "src/hooks/useSmartContractReadCalls/useSmartContractReadCalls";
