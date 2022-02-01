3544469f7e1eb996659a62d0c3151661dde7d146
Danny Delott <dannydelott@gmail.com>
2022-02-01 11:12:08 -0800

    Move logic out of hooks directory

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ export * from "src/base/TransactionError";
 export * from "src/base/TransactionStatus";
 export * from "src/base/Unpacked";
 
-export * from "src/hooks/matchSmartContractReadCallQuery/matchSmartContractReadCallQuery";
+export * from "src/utils/matchSmartContractReadCallQuery/matchSmartContractReadCallQuery";
+
 export * from "src/hooks/useSmartContractEvents/useSmartContractEvents";
 export * from "src/hooks/useSmartContractReadCall/useSmartContractReadCall";
 export * from "src/hooks/useSmartContractReadCalls/useSmartContractReadCalls";
 export * from "src/hooks/useSmartContractTransaction/useSmartContractTransaction";
 export * from "src/hooks/useSmartContractEstimateGas/useSmartContractEstimateGas";
+
 export * from "src/types";
